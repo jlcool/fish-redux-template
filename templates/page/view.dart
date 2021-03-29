f6efad4b40d1f23db1bff7e670e7ea42db24a9d6
jlcool <jiangliang.net@gmail.com>
2021-03-29 22:35:53 +0800

    添加listview使用说明

--- templates/page/view.dart
+++ templates/page/view.dart
@@ -4,6 +4,14 @@ import 'package:flutter/material.dart';
 import 'action.dart';
 import 'state.dart';
 
+// final ListAdapter adapter = viewService.buildAdapter();
+// ListView.separated(
+//                   separatorBuilder: (BuildContext context, int index) =>
+//                       Divider(
+//                         height: 1,
+//                       ),
+//                   itemBuilder: adapter.itemBuilder,
+//                   itemCount: adapter.itemCount)
 Widget buildView($namePageState state, Dispatch dispatch, ViewService viewService) {
   return Container();
 }
