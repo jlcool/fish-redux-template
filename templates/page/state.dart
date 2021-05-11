0a7ea3b7034ab97a68312151f1e53b37fd10a0ab
jiangliang <jiangliang.net@gmail.com>
2021-05-11 22:54:32 +0800

    slots 实现

--- templates/page/state.dart
+++ templates/page/state.dart
@@ -2,14 +2,14 @@ import 'package:fish_redux/fish_redux.dart';
 
 //使用ListView需要这个继承方法extends MutableSource
 class $namePageState implements GlobalBaseState, Cloneable<$namePageState> {
-
+  // $nameComponentState $nameComponentState;
   @override
   $namePageState clone() {
     return $namePageState();
   }
 
   // List<$nameComponentState> datas;
-  
+
   // @override
   // StoreModel store;
 
@@ -29,3 +29,18 @@ class $namePageState implements GlobalBaseState, Cloneable<$namePageState> {
 $namePageState initState(Map<String, dynamic> args) {
   return $namePageState();
 }
+
+// class $nameConnector extends ConnOp<$namePageState, $nameComponentState>
+//     with ReselectMixin<$namePageState, $nameComponentState> {
+//   @override
+//   $nameComponentState computed($namePageState state) {
+//     var $name = state.$nameComponentState.clone();
+
+//     return $name;
+//   }
+
+//   @override
+//   void set($namePageState state, $nameComponentState subState) {
+//     state.$nameComponentState = subState;
+//   }
+// }
